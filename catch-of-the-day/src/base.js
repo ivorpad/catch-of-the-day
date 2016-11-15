e9c52a7a122e300f47a2ec15aba310c397a7423c
ivorpad <ivorjpc@gmail.com>
2016-11-15 00:22:34 -0400

    Changed apiKey

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,7 +1,7 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-  apiKey: "SECRET",
+  apiKey: "AIzaSyDVd1ecGWWm05gz7GEeNGan0gbf-5VC0Hg",
   authDomain: "catch-of-the-day-ivor.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-ivor.firebaseio.com",
 });
